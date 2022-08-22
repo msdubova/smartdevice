72c59eed655e50eb6de64d662e3d3a26d87c78d9
Olga Dubova <ms.olga.dubovaya@gmail.com>
2022-08-23 01:48:43 +0300

    Маска

--- source/js/main.js
+++ source/js/main.js
@@ -57,9 +57,14 @@ const modalBlock = document.querySelector('.modal');
 const callButton = document.querySelector('.header__button');
 const closeButton = modalBlock.querySelector('.modal__button-close');
 const body = document.querySelector('.page');
+const overlay = document.querySelector('.modal__overlay');
+const nameInput = document.querySelector('#modal-name');
 
 
 function closeModal () {
+  overlay.removeEventListener('click', function() {
+    closeModal();
+  })
   modalBlock.classList.add('visually-hidden');
   body.classList.remove('scroll-lock');
 }
@@ -67,8 +72,12 @@ function closeModal () {
 function openModal () {
   modalBlock.classList.remove('visually-hidden');
   body.classList.add('scroll-lock');
-
+  nameInput.focus();
   closeButton.addEventListener('click', function(){
+    closeModal();
+  });
+
+  overlay.addEventListener('click', function(){
     closeModal();
   })
 }
@@ -80,3 +89,44 @@ function openModal () {
 }
 
 controlModal();
+
+// маска для телефона
+
+  function maskPhone() {
+    const elems = document.querySelectorAll('input[type="tel"]');
+
+    function mask(event) {
+      const keyCode = event.keyCode;
+      const template = '+7 (___) ___-__-__';
+      const def = template.replace(/\D/g, '');
+      const val = event.target.value.replace(/\D/g, '');
+
+      let i = 0;
+      let newValue = template.replace(/[_\d]/g, function (a) {
+        return i < val.length ? val.charAt(i++) || def.charAt(i) : a;
+      });
+      i = newValue.indexOf('_');
+      if (i !== -1) {
+        newValue = newValue.slice(0, i);
+      }
+      let reg = template.substr(0, event.target.value.length).replace(/_+/g,
+          function (a) {
+            return '\\d{1,' + a.length + '}';
+          }).replace(/[+()]/g, '\\$&');
+      reg = new RegExp('^' + reg + '$');
+      if (!reg.test(event.target.value) || event.target.value.length < 5 || keyCode > 47 && keyCode < 58) {
+        event.target.value = newValue;
+      }
+      if (event.type === 'blur' && event.target.value.length < 5) {
+        event.target.value = ' ';
+      }
+    }
+
+    for (const elem of elems) {
+      elem.addEventListener('input', mask);
+      elem.addEventListener('focus', mask);
+      elem.addEventListener('blur', mask);
+    }
+  }
+
+  maskPhone();
